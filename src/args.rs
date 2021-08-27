97696f887b36f91bf069fce7b86576111148f898
LtPeriwinkle <not.yet.periwinkle@gmail.com>
2021-08-26 19:31:01 -0700

    initial commit

---
+++ src/args.rs
@@ -0,0 +1,42 @@
+use std::env;
+use std::path::PathBuf;
+use std::str::FromStr;
+
+pub enum Action {
+    Interpret,
+    Compile,
+    Check
+}
+
+impl FromStr for Action {
+    type Err = String;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        let s = s.to_ascii_lowercase();
+        match s.as_str() {
+            "interpret" | "i" => Ok(Action::Interpret),
+            "compile" | "cc" => Ok(Action::Compile),
+            "check" | "chk" => Ok(Action::Check),
+             _ => Err("Not a valid action".into())
+        }
+    }
+}
+
+pub struct Config {
+    pub act: Action,
+    pub infile: Option<PathBuf>,
+}
+
+impl Config {
+    pub fn get() -> Result<Self, String> {
+        let args = env::args();
+        let mut cfg = Config {
+            act: Action::Interpret,
+            infile: None
+        };
+        let mut args = args.skip(1);
+        if let Ok(a) = Action::from_str(&args.next().ok_or("Not enough arguments.".to_owned())?) {
+            cfg.act = a;
+        }
+        Ok(cfg)
+    }
+}
---
+++ src/main.rs
@@ -0,0 +1,21 @@
+mod args;
+use args::*;
+
+const USAGE: &str = "compile or interpret tas-script\n
+USAGE:
+    tasc SUBCOMMAND INPUT\n
+INPUT:
+    The file to be compiled or interpreted.\n
+SUBCOMMAND:
+    interpret, i  interpret INPUT in real time
+    verify, v     check that INPUT uses valid syntax
+    compile, c    compile INPUT to an executable\n";
+
+fn main() {
+    let cfg = Config::get();
+    if let Err(e) = cfg {
+        println!("{}", e);
+        println!("{}", USAGE);
+        std::process::exit(1);
+    }
+}
