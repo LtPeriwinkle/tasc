3f9ad88f2d8163120d64b78e291e2468475aca48
LtPeriwinkle <not.yet.periwinkle@gmail.com>
2021-09-04 16:05:36 -0700

    Add TasError::Vigem variant

--- src/lib.rs
+++ src/lib.rs
@@ -14,17 +14,45 @@ mod parse;
 
 #[derive(Debug)]
 pub enum TasError {
-    Parse { l: usize, c: usize, e: &'static str, p: PathBuf},
-    Syntax { l: usize, c: usize, e: &'static str, p: PathBuf},
-    Fs { e: String },
+    Parse {
+        l: usize,
+        c: usize,
+        e: &'static str,
+        p: PathBuf,
+    },
+    Syntax {
+        l: usize,
+        c: usize,
+        e: &'static str,
+        p: PathBuf,
+    },
+    Fs {
+        e: String,
+    },
+    Vigem {
+        e: String,
+    },
 }
 
 impl Display for TasError {
     fn fmt(&self, f: &mut Formatter) -> Result<(), std::fmt::Error> {
         let rep = match self {
-            TasError::Parse { l, c, e, p } => format!("\x1b[31;1mParse Error:\x1b[0m \x1b[1m{}\x1b[0m\n\t->{}:{}:{}", e, p.display(), l, c),
-            TasError::Syntax { l, c, e, p } => format!("\x1b[31;1mSyntax Error:\x1b[0m \x1b[1m{}\x1b[0m\n\t->{}:{}:{}", e, p.display(), l, c),
+            TasError::Parse { l, c, e, p } => format!(
+                "\x1b[31;1mParse Error:\x1b[0m \x1b[1m{}\x1b[0m\n\t->{}:{}:{}",
+                e,
+                p.display(),
+                l,
+                c
+            ),
+            TasError::Syntax { l, c, e, p } => format!(
+                "\x1b[31;1mSyntax Error:\x1b[0m \x1b[1m{}\x1b[0m\n\t->{}:{}:{}",
+                e,
+                p.display(),
+                l,
+                c
+            ),
             TasError::Fs { e } => format!("{}", e),
+            TasError::Vigem { e } => format!("ViGEm Error: {}", e),
         };
         write!(f, "{}", rep)
     }
